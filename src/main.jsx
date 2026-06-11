5af81a60ee0d9be8f239228f91d941643b6567e4
dineshyelle1-sudo <dineshyelle1@gmail.com>
2026-06-12 00:52:45 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NHCDashboard from './NHCDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NHCDashboard />
+  </React.StrictMode>
+)
