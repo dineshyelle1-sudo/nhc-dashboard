4d5447de3e1bf6f182b5fa9b03b5da75ad9ffd66
dineshyelle1-sudo <dineshyelle1@gmail.com>
2026-06-12 01:28:21 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NHCDashboard from './NHCDashboard.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NHCDashboard />
-  </React.StrictMode>
-)
